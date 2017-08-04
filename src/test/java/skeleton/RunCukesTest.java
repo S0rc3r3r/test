e72bfe29f55dd1b7664ce729f2fc48eefbe71c56
Tanasoiu Bogdan <bogdan.tanasoiu@iquestgroup.com>
2017-08-04 11:12:12 +0300

    aa

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -6,6 +6,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty", "html:target/html", "json:cucumber.json" }, monochrome = true)
+@CucumberOptions(plugin = { "pretty", "html:target/html", "json:target/cucumber.json" }, monochrome = true)
 public class RunCukesTest {
 }
